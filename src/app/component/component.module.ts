bb64e6cd83a00d6b3d930d0eff7157fac3c57ec6
Ayesha Khalid <ayeshakhalid289@gmail.com>
2023-07-01 15:57:47 +0500

    creates banner

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule
