0227782f9123b2de237693fa371d32dd06c07eb3
Ayesha Khalid <ayeshakhalid289@gmail.com>
2023-07-01 20:05:31 +0500

    creates about section

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  isNavbarOpen: boolean = false;
 
-  ngOnInit(): void {
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
   }
 
 }
